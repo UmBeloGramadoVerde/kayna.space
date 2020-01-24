fb40a6279709d9bbe283e7c0bff082d690407c9c
UmBeloGramadoVerde <kaynarodrigues@live.com>
2020-01-24 21:47:10 +0000

    Address bar fix 2.0

--- src/modules/repulsion-handler/RepulsionHandler.js
+++ src/modules/repulsion-handler/RepulsionHandler.js
@@ -32,23 +32,15 @@ export default class RepulsionHandler extends Component {
     window.addEventListener('resize', (ev) => {
       let innerWidth = ev.target.innerWidth;
       let innerHeight = ev.target.innerHeight;
-      let diff = this.previousHeigth - innerHeight;
-      let absDiff = Math.abs(diff);
+      let diff = Math.abs(this.previousHeigth - innerHeight);
       document.getElementById('diff').innerText = diff;
       this.previousHeigth = innerHeight;
-      if ((absDiff !== 56) && (absDiff !== 75) && (absDiff !== 76)) {
-        /* this.previousHeigth > innerHeight */
-        /* Screen got smaller */
-        /* URL bar is shown */
-        if (diff > 0) {
-          this.bg.current.resize(innerWidth + absDiff, innerHeight + absDiff);
-        }
-        /* is.previousHeigth < innerHeight */
-        /* Screen got bigger */
-        /* URL bar is hidden */
-        else if (diff < 0) {
-          this.bg.current.resize(innerWidth + absDiff, innerHeight + absDiff);
-        }
+      /* I have to map all the url bar sizes */
+      /* This is a dumb implementation, there is probably a smart way of getting the size */
+      if ((diff !== 56) && (diff !== 75) && (diff !== 76) && (diff !== 0)) {
+        this.bg.current.resize(innerWidth, innerHeight);
+      } else {
+        document.querySelectorAll('.Background')[0].style.height = `calc(100vh + ${diff}px)`
       }
     });
   }
