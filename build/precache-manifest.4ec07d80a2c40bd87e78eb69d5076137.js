b890cf588d625bf766c325557e7fe8a0ed136602
UmBeloGramadoVerde <kaynarodrigues@live.com>
2020-02-10 01:37:58 +0000

    Run the build locally

---
+++ build/precache-manifest.4ec07d80a2c40bd87e78eb69d5076137.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3550241a21b10b21f0038249b14b745a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c85e618cb91033c706d1",
+    "url": "/static/css/main.ddcc08e5.chunk.css"
+  },
+  {
+    "revision": "5a75a36ad0e305b3bdde",
+    "url": "/static/js/2.fc18f15a.chunk.js"
+  },
+  {
+    "revision": "c85e618cb91033c706d1",
+    "url": "/static/js/main.e9802672.chunk.js"
+  },
+  {
+    "revision": "7ad182e6d5cf6acfcf18",
+    "url": "/static/js/runtime-main.19717a6f.js"
+  }
+]);
