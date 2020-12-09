540b72b1436785012a08f622f2b97b99b5310170
UmBeloGramadoVerde <kaynarodrigues@live.com>
2020-12-09 16:37:51 +0000

    not cimmited changes, because im dumb

---
+++ build/precache-manifest.4ea6ea160d982443e750b4aa0c8b3a72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d12a18113465f62a97a9bb0b44d5660c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6a4805f72c28cbb4dcf",
+    "url": "/static/css/main.ffe6810d.chunk.css"
+  },
+  {
+    "revision": "069426d0b520dfc8b51d",
+    "url": "/static/js/2.9915aa2a.chunk.js"
+  },
+  {
+    "revision": "a6a4805f72c28cbb4dcf",
+    "url": "/static/js/main.d2280185.chunk.js"
+  },
+  {
+    "revision": "7ad182e6d5cf6acfcf18",
+    "url": "/static/js/runtime-main.19717a6f.js"
+  }
+]);
