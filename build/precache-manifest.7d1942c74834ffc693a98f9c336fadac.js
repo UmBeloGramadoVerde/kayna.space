947c9e4089e9062e6d8c233c0380061c69608264
UmBeloGramadoVerde <kaynarodrigues@live.com>
2022-01-28 12:19:24 +0000

    forgot the god damn build

---
+++ build/precache-manifest.7d1942c74834ffc693a98f9c336fadac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13090ff60f61cce5f8d061fd6c7eb0ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb9137791556a37c6afa",
+    "url": "/static/css/main.8a2a5645.chunk.css"
+  },
+  {
+    "revision": "d2c3c99ee3a2997aa0ca",
+    "url": "/static/js/2.0d0c3597.chunk.js"
+  },
+  {
+    "revision": "bb9137791556a37c6afa",
+    "url": "/static/js/main.22eca87d.chunk.js"
+  },
+  {
+    "revision": "7ad182e6d5cf6acfcf18",
+    "url": "/static/js/runtime-main.19717a6f.js"
+  }
+]);
